bd8d4cfe783696c6e03907436c5e90284eda1648
Marcial Soto <marcialsotosanchez@gmail.com>
2016-10-14 15:33:42 -0500

    Gallery

--- wp-content/themes/pssa/templates/masshead.php
+++ wp-content/themes/pssa/templates/masshead.php
@@ -1,9 +1,9 @@
 <?php use Roots\Sage\Titles; ?>
 
 <?php 
-	global $post; 
-	$upload_dir = wp_upload_dir();
-	$default_masshead_uri = $upload_dir['baseurl'].'/2013/11/masshead-sierra-sur.jpg';
+	//global $post; 
+	//$upload_dir = wp_upload_dir();
+	//$default_masshead_uri = $upload_dir['baseurl'].'/2013/11/masshead-sierra-sur.jpg';
 ?>
 
 <?php if ( !is_404() && has_post_thumbnail( $post->ID ) ){ ?>
